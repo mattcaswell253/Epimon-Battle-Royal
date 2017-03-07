f1fcb0656b851b99d70c35b66a30cac840a99ae3
Brandon Cooper, Kory Skarbek, Matt Caswell and Sandro Mijatovic <brandon+kory+matt+sandro@>
2017-03-06 16:23:07 -0800

    add get for index, character-select and arena html pages

---
+++ Modules/HomeModules.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using Nancy;
+using System;
+using Nancy.ViewEngines.Razor;
+
+namespace Epimon
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = _ => {
+                return View["index.cshtml"];
+            };
+
+            Get["/character-select"] = _ => {
+                List<Character> allCharacters = Character.GetAll();
+                return View["character-select.cshtml", allCharacters];
+            };
+
+            Get["/arena"] = _ => {
+                Dictionary<string, object> model = new Dictionary<string, object>();
+                List<Character> allCharacters = Character.GetAll();
+                List<Move> allMoves = Move.GetAllMoves();
+                model.Add("characters", allCharacters);
+                model.Add("moves", allMoves);
+                return View["arena.cshtml", model];
+            };
+        }
+    }
+}
