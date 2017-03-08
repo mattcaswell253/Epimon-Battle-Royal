13244f08417a329c578b6189e4823131672af159
brandon cooper, kory skarbek, matt caswell and sandro mijatovic <brandon+kory+matt+sandro@>
2017-03-08 08:59:29 -0800

    fix merge issues

--- Modules/HomeModules.cs
+++ Modules/HomeModules.cs
@@ -27,38 +27,46 @@ namespace Epimon
                 Character.player2 = Character.Find(Request.Form["character2-id"]);
                 int health1 = Character.player1.GetHealth();
                 int health2 = Character.player2.GetHealth();
-                // Character character1Attacked = Character.Find(Request.Form["character1-id"]);
-                // Character character2Attacked = Character.Find(Request.Form["character2-id"]);
                 model.Add("p1health", health1);
                 model.Add("p2health", health2);
                 model.Add("character1", character1);
                 model.Add("character2", character2);
                 model.Add("character1Moves", character1Moves);
                 model.Add("character2Moves", character2Moves);
-                // model.Add("character1Attacked", character1Attacked);
-                // model.Add("character2Attacked", character2Attacked);
                 return View["arena.cshtml", model];
             };
-            Post["/attack"] = _ => {
+            Post["/attack1"] = _ => {
                 Dictionary<string, object> model = new Dictionary<string, object>();
                 Character character1 = Character.Find(Request.Form["character1-id"]);
                 Character character2 = Character.Find(Request.Form["character2-id"]);
                 List<Move> character1Moves = character1.GetMoves();
-                List<Move> character2Moves = character2.GetMoves();
-                // Character.player1 = Character.Find(Request.Form["character1-id"]);
-                // Character.player2 = Character.Find(Request.Form["character2-id"]);
+                // List<Move> character2Moves = character2.GetMoves();
                 int health1 = Character.player1.GetHealth();
                 int health2 = Character.player2.GetHealth();
-                Character.player1.Attack1(Move.Find(Request.Form["character2Attack"]));
                 Character.player2.Attack2(Move.Find(Request.Form["character1Attack"]));
                 model.Add("p1health", health1);
                 model.Add("p2health", health2);
                 model.Add("character1", character1);
                 model.Add("character2", character2);
                 model.Add("character1Moves", character1Moves);
+                // model.Add("character2Moves", character2Moves);
+                return View["arena.cshtml", model];
+            };
+            Post["/attack2"] = _ => {
+                Dictionary<string, object> model = new Dictionary<string, object>();
+                Character character1 = Character.Find(Request.Form["character1-id"]);
+                Character character2 = Character.Find(Request.Form["character2-id"]);
+                // List<Move> character1Moves = character1.GetMoves();
+                List<Move> character2Moves = character2.GetMoves();
+                int health1 = Character.player1.GetHealth();
+                int health2 = Character.player2.GetHealth();
+                Character.player1.Attack1(Move.Find(Request.Form["character2Attack"]));
+                model.Add("p1health", health1);
+                model.Add("p2health", health2);
+                model.Add("character1", character1);
+                model.Add("character2", character2);
+                // model.Add("character1Moves", character1Moves);
                 model.Add("character2Moves", character2Moves);
-                // model.Add("character1Attacked", character1Attacked);
-                // model.Add("character2Attacked", character2Attacked);
                 return View["arena.cshtml", model];
             };
 
